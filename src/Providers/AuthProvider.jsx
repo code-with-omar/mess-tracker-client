e1cf8728473a8d884a3d6090d0ac28c947dd6ce1
Omar Faruk <omarfaruk65142@gmail.com>
2024-09-15 23:08:39 +0600

    Create firebase AuthProvider and aslo create user,signIn user, logOut auth

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,49 @@
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+import { createContext, useEffect, useState } from "react";
+const auth = getAuth(app)
+export const AuthContext = createContext()
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    // create user with email and password 
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    //sign in with email and password
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    // logout 
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false)
+
+        })
+        return () => {
+            unsubscribe()
+        }
+
+    }, [])
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
