a3bb84d6df3dd6166ec7b77e4f9afad2d52b315d
Omar Faruk <omarfaruk65142@gmail.com>
2024-09-16 03:18:37 +0600

    User private route create

---
+++ src/Routers/PrivateRoute.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
+
+const PrivateRouter = ({ children }) => {
+    const { user, loading } = useContext(AuthContext)
+    const location = useLocation()
+    if (loading) {
+        return (
+            <div className="grid grid-flow-col place-items-center h-screen w-1/6 mx-auto">
+                <progress className="progress w-56"></progress>
+            </div>
+
+        )
+
+
+    }
+    if (user) {
+        return children
+    }
+    return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+};
+
+export default PrivateRouter;
--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -1,29 +1,26 @@
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Login from "../Pages/Main/Login/Login";
 import SignUp from "../Pages/Main/SignUp/SignUp";
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-          path: '/',
-          element: <Login></Login>
-        },
-        {
-          path: '/login',
-          element: <Login></Login>
-        },
-        {
-          path: '/signUp',
-          element: <SignUp></SignUp>
-        },
-       
-      ]
-    },
-  
-  
-  ]);
+import Dashboard from "../Layout/Dashboard";
+import PrivateRouter from "./PrivateRoute";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <PrivateRouter>
+        <Dashboard />
+      </PrivateRouter>
+    ),
+    children: [],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signUp",
+    element: <SignUp />,
+  },
+]);
