872d63b6f5d305863861938c89ae41f003f3b74a
Omar Faruk <omarfaruk65142@gmail.com>
2024-09-19 21:53:35 +0600

    Make admin create functinility

--- src/Pages/Main/AllMembers/AllMembers.jsx
+++ src/Pages/Main/AllMembers/AllMembers.jsx
@@ -3,10 +3,42 @@ import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { FaUsers } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import useUsers from "../../../Hooks/useUsers";
+import { RiAdminLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AllMembers = () => {
-    const [users] = useUsers()
+    const [users,refetch] = useUsers()
+    const axiosSecure=useAxiosSecure()
+    const rule = (user) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: `You make ${user.name} as admin?`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.patch(`/users/admin/${user._id}`)
+                    .then(res => {
+                        console.log(res.data)
+                        if (res.data.modifiedCount > 0) {
+                            Swal.fire({
+                                title: "Success!",
+                                text: `Now ${user.name} is a admin`,
+                                icon: "success"
+                            });
+                            refetch()
+                        }
+                    })
+
+            }
+
+        });
+
+    }
     return (
         <div>
             <div className="flex justify-between">
@@ -20,7 +52,8 @@ const AllMembers = () => {
                             <th>#</th>
                             <th>Name</th>
                             <th>Rule</th>
-                            <th className="text-right">Action</th>
+
+                            <th className="text-right">Details</th>
                         </tr>
                     </thead>
                     <tbody className="text-[#737373] text-lg">
@@ -29,8 +62,12 @@ const AllMembers = () => {
                                 <tr key={user._id} className="border-b-[.5px] border-[#E8E8E8]">
                                     <td>{index + 1}</td>
                                     <td>{user.name}</td>
+                                    <td>
+                                        {
+                                            user.role === 'admin' ? "Admin" : <button onClick={() => rule(user)} className="btn btn-ghost btn-xs text-lg text-white bg-[#D1A054] rounded-md w-12 h-12 hover:bg-white hover:text-[#B91C1C] transition-colors "><FaUsers className="text-3xl "></FaUsers></button>
+                                        }
 
-                                    <td>Admin</td>
+                                    </td>
                                     <td className="text-right">
                                         <Link to={`/details/${user.email}`}>
                                             <button
@@ -38,7 +75,7 @@ const AllMembers = () => {
                                                 className="btn btn-success px-4 py-2 rounded-lg bg-gray-100 border border-gray-200  font-medium focus:outline-none focus:border-gray-400 focus:bg-white text-base md:text-lg"
                                             >
                                                 Details
-                                                
+
                                             </button>
                                         </Link>
                                     </td>
