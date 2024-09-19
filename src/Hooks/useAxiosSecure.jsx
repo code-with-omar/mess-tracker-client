bc35be50623b6ba97c99a24fe7057faf239419d2
Omar Faruk <omarfaruk65142@gmail.com>
2024-09-19 21:32:13 +0600

    Verify jwt token

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,8 +1,37 @@
 import axios from "axios";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider";
 const axiosSecure = axios.create({
     baseURL: 'https://mess-traker-server.vercel.app/',
 });
 const useAxiosSecure = () => {
+    const navigate = useNavigate()
+    const { logOut } = useContext(AuthContext)
+    //request interceptors to secure ao add authorization header for every
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        config.headers.authorization = `Bearer ${token}`
+        return config
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    })
+
+    // interceptors for 401 and 403 status
+    axiosSecure.interceptors.response.use(function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+    }, async (error) => {
+        const status = error.response.status;
+        if (status === 401 || status === 403) {
+            await logOut()
+            navigate('/login')
+        }
+        return Promise.reject(error);
+    });
+
     return axiosSecure
 };
 
